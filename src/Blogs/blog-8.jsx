1ba22becaebceb236be04f0b9bb92b06708f7260
hpknollenberg <hpknollenberg@gmail.com>
2024-05-03 09:44:36 -0400

    styling touch ups, fixed blog text

--- src/Blogs/blog-8.jsx
+++ src/Blogs/blog-8.jsx
@@ -4,7 +4,6 @@ import Menu from "../Menu"
 
 const styles = {
   text: {
-    fontWeight: 'bold',
     backgroundColor: 'white',
     margin: '10px',
     borderRadius: '5px',
@@ -24,15 +23,15 @@ function Title() {
 function Section() {
   return (
     <div className="blog-entry" style={{...styles.text}}>
-      <h3>Do you think JavaScript Frameworks and Libraries are easy or hard to use? Why?</h3>
+      <h4>Do you think JavaScript Frameworks and Libraries are easy or hard to use? Why?</h4>
       <p>They are difficult for me to wrap my head around at first; however, I can see that 
         they do make things simpler or at least easier to track, especially when projects become
         more and more complex. I very much appreciate the more modular aspects of React over 
         vanilla JavaScript.</p><br></br>
-      <h3>What Frameworks or Libraries are you interested in learning more about?</h3>
+      <h4>What Frameworks or Libraries are you interested in learning more about?</h4>
       <p>I'm not teribly familiar with a lot of frameworks or libraries, but after a quick search,
         Anime.js sounds like an interesting library. It is used to add animations to sites and apps.</p><br></br>
-      <h3>Find one emerging JavaScript tool that you would like to learn more about or use in your final project.</h3>
+      <h4>Find one emerging JavaScript tool that you would like to learn more about or use in your final project.</h4>
       <p>Vue looks like an interesting tool. It is said to be similar to React, but is gaining traction due 
         to its simplicity and level of performance on all browsers.</p><br></br>
     </div>
