3845d19cb1a752d8b4e1b7f4bd94a290d2a48b83
hpknollenberg <hpknollenberg@gmail.com>
2024-05-06 16:18:46 -0400

    Added connect 4 project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,6 +3,7 @@ import Restaurant from "./Project Cards/Project-Restaurant.jsx"
 import Weather from "./Project Cards/Project-WeatherApp.jsx"
 import TicTacToe from "./Project Cards/Project-Tic-Tac-Toe.jsx"
 import ToDoList from "./Project Cards/Project-To-Do-List.jsx"
+import Connect4 from "./Project Cards/Project-Connect-4.jsx"
 
 const styles = {
     card: {
@@ -19,6 +20,7 @@ function Projects() {
         <div className="row">
           <ToDoList />
           <Restaurant />
+          <Connect4 />
           <TicTacToe />
           <Weather />
         </div>
