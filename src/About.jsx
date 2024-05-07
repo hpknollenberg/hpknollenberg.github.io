76018bded84ed03994bf613455aeb9d1523f719c
hpknollenberg <hpknollenberg@gmail.com>
2024-05-07 14:02:57 -0400

    added box shadow to cards

--- src/About.jsx
+++ src/About.jsx
@@ -37,12 +37,12 @@ function Title() {
 function Section() {
   return (
     <div className="blog-entry" style={{...styles.text}}>
-      <p>My name is Henry Knollenberg. I am a web developer familiar with HTML, CSS, JavaScript, and React. 
+      <p>My name is Henry Knollenberg. I am a web developer familiar with HTML, CSS, JavaScript, React, and Python. 
         <br></br>I am a student of Awesome Inc.'s Web-Developer Bootcamp.</p>
       <p>Before becoming a web developer, I received an MFA in Creative Writing from the University of Kentucky.</p>
       <p>My most recent job was to train AI responses concerning writing ability as well as factuality. 
         <br></br> I have also worked in Academia, Data Entry, Retail, and Construction.</p>
-      <p>I currently live in Lexington, Kentucky, with my girlfriend Allegra and our dog Lucky.</p>
+      <p>I currently live in Lexington, Kentucky, with my fiancée Allegra and our dog Lucky.</p>
     </div>
   )
 }
